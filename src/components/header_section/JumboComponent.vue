f2f83d03f56dca79d431e5dd2a254d084e00f20c
mattialicausi <licausivitomattia@gmail.com>
2022-11-23 15:26:18 +0100

    Aggiunto transition al jumbo al caricamento della pagina

--- src/components/header_section/JumboComponent.vue
+++ src/components/header_section/JumboComponent.vue
@@ -1,9 +1,14 @@
 <template>
     <div class="myWrapper">
         <div class="container-info-jumbo text-center">
-            <h1 class="title-jumbo">Contemporary Ideas</h1>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Excepturi deserunt harum est tenetur corporis, dolorum quas pariatur voluptate non consequuntur autem nesciunt commodi ullam in error mollitia?</p>
-            <button class="myBTN text-uppercase">Register now</button>
+
+                <Transition name="slide-fade">
+                    <div v-if="enterFade">
+                        <h1 class="title-jumbo">Contemporary Ideas</h1>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Excepturi deserunt harum est tenetur corporis, dolorum quas pariatur voluptate non consequuntur autem nesciunt commodi ullam in error mollitia?</p>
+                        <button class="myBTN text-uppercase">Register now</button>
+                    </div>
+                </Transition>
         </div>
     </div>
 
@@ -14,11 +19,28 @@
     export default {
         name: 'JumboComponent',
 
+        data () {
+            return {
+                enterFade: false,
+            }
+        },
+
+        methods: {
+            showInfo() {
+                this.enterFade = true;
+            }
+        },
+
+        mounted () {
+            this.showInfo();
+        }
+
     }
 </script>
 
 <style lang="scss" scoped>
 @import '../../assets/style/general.scss';
+@import "https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css";
 
     .myBTN {
         background-color: rgb(101, 195, 249);
@@ -40,5 +62,19 @@
         margin: 0 auto;
         line-height: 2rem;
     }
+
+    .slide-fade-enter-active {
+  transition: all 1s ease-out;
+}
+
+.slide-fade-leave-active {
+  transition: all 0.8s cubic-bezier(1, 0.5, 0.8, 1);
+}
+
+.slide-fade-enter-from,
+.slide-fade-leave-to {
+  transform: translateX(20px);
+  opacity: 0;
+}
 
 </style>
